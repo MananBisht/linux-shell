ffa8a429439f11e8c5fb97a67c34566e9fc58b52
MananBisht <bishtmanan205@gmail.com>
2026-07-23 13:54:40 -0400

    file descriptors implemented

--- src/main.c
+++ src/main.c
@@ -13,6 +13,57 @@ enum RedirectType
     APPEND
 };
 
+typedef struct
+{
+    char* output_file ;
+    char* input_file ;
+    enum RedirectType rdir_type;
+} RedirectInfo ;
+
+int redirect_output(const RedirectInfo *redir){
+    if(redir->output_file == NULL)   return 0 ;
+
+    int fd ;
+    if(redir->rdir_type == TRUNCATE)
+        fd = open(redir->output_file,O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    else if(redir->rdir_type == APPEND)
+        fd = open(redir->output_file,O_CREAT | O_WRONLY | O_APPEND, 0644);
+                
+    if(fd == -1 ){
+        perror("file error");
+        return -1 ;
+    }
+
+    if(dup2(fd,STDOUT_FILENO) == -1){
+        perror("dup2 error");
+        close(fd) ;
+        return -1 ;
+    }
+
+    close(fd)  ;
+    return 1 ;
+}
+
+int redirect_input(const RedirectInfo *redir){
+    if(redir->input_file == NULL)   return 0;
+    int fd ;
+    fd = open(redir->input_file, O_RDONLY) ;
+                
+    if(fd == -1 ){
+        perror("file error");
+        exit(1) ;
+    }
+
+    if(dup2(fd,STDIN_FILENO) == -1){
+        perror("dup2 error");
+        close(fd) ;
+        return -1 ;                   
+    }
+
+    close(fd)  ;
+    return 1 ;
+}
+
 
 int main(){
     char cmd[1024] = "\0" ;
@@ -25,7 +76,12 @@ int main(){
     getcwd(s,sizeof(s)) ;
 
     while(1){
-        char* filename = NULL ;
+        RedirectInfo redir ;
+        redir.input_file = NULL ;
+        redir.output_file = NULL ;
+        redir.rdir_type = NONE ;
+        
+
         if(strcmp(s,p_dir)==0)
             printf("myshell> ") ;
         else{
@@ -50,22 +106,40 @@ int main(){
         args[tokenc] = NULL ;
 
         tokenc = 0 ;
-        enum RedirectType type = NONE ;
+        int error = 0 ;
 
         while(args[tokenc]){
             if(strcmp(args[tokenc],">")==0){
+                if(args[tokenc+1] == NULL){
+                    printf("error :no file\n") ;
+                    error = 1 ;
+                    break ;
+                }
                 args[tokenc] = NULL ;
-                filename = args[tokenc+1] ;
-                type = TRUNCATE ;
-                break ;
+                redir.output_file = args[tokenc+1] ;
+                redir.rdir_type = TRUNCATE ;
             } else if(strcmp(args[tokenc],">>")==0){
+                if(args[tokenc+1] == NULL){
+                    printf("error :no file\n") ;
+                    error = 1 ;
+                    break ;
+                }
                 args[tokenc] = NULL ;
-                filename = args[tokenc + 1] ;
-                type = APPEND ;
-                break ;
-            } 
+                redir.output_file = args[tokenc + 1] ;
+                redir.rdir_type = APPEND ;
+            } else if(strcmp(args[tokenc],"<") == 0){
+                if(args[tokenc+1] == NULL){
+                    printf("error :no file\n") ;
+                    error = 1 ;
+                    break ;
+                }
+                args[tokenc] = NULL ;
+                redir.input_file = args[tokenc+1] ;
+            }
             tokenc ++ ;
         }
+
+        if(error == 1 )     continue ;
 
         // implementing cd command
 
@@ -80,40 +154,24 @@ int main(){
             continue ;
         }
 
-        if(strcmp(args[0] ,"exit")==0)   exit(1) ; 
+        if(strcmp(args[0] ,"exit")==0)   break ;; 
 
 
         pid_t pid = fork() ;
 
         if(pid == 0){
 
-            // implementing basic file descriptor structure
+            // implementing file descriptor
 
-            if(filename!=NULL){
-		int fd ;
-                if(type == TRUNCATE)
-                    fd = open(filename,O_CREAT | O_WRONLY | O_TRUNC, 0644);
-                else if(type == APPEND)
-                    fd = open(filename,O_CREAT | O_WRONLY | O_APPEND, 0644);
-                
-                if(fd == -1){
-                    perror("file error");
-                    exit(1) ;
-                }
-                
-                if(dup2(fd,STDOUT_FILENO) == -1){
-                    perror("dup2 error");
-                    close(fd) ;
-                    exit(1) ;
-                }
+            // if (redirect_output(&redir) == -1)
+            //     _exit(EXIT_FAILURE); 
+            
+            // if (redirect_input(&redir) == -1)
+            //     _exit(EXIT_FAILURE); 
 
-                close(fd) ;
+            redirect_output(&redir);
+            redirect_input(&redir);
 
-                if(execvp(args[0],args) == -1){
-                    perror("execvp error");
-                    exit(1) ;
-                }
-            }
             if(execvp(args[0],args) == -1 ){
                 perror("execvp fail") ;
                 exit(1);
