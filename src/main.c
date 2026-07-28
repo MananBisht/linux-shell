d65f6aeccdf5ea8bc3131ff86eb4cd16e80c04a0
MananBisht <bishtmanan205@gmail.com>
2026-07-28 13:50:23 -0400

    working shell

--- src/main.c
+++ src/main.c
@@ -1,3 +1,9 @@
+#include "redirect.h"
+#include "lexer.h"
+#include "parser.h"
+#include "executor.h"
+#include "builtin.h"
+
 #include<stdio.h>
 #include<stdbool.h>
 #include<string.h>
@@ -6,87 +12,23 @@
 #include<stdlib.h>
 #include<fcntl.h>
 
-enum RedirectType
-{
-    NONE,
-    TRUNCATE,
-    APPEND
-};
-
-typedef struct
-{
-    char* output_file ;
-    char* input_file ;
-    enum RedirectType rdir_type;
-} RedirectInfo ;
-
-int redirect_output(const RedirectInfo *redir){
-    if(redir->output_file == NULL)   return 0 ;
-
-    int fd ;
-    if(redir->rdir_type == TRUNCATE)
-        fd = open(redir->output_file,O_CREAT | O_WRONLY | O_TRUNC, 0644);
-    else if(redir->rdir_type == APPEND)
-        fd = open(redir->output_file,O_CREAT | O_WRONLY | O_APPEND, 0644);
-                
-    if(fd == -1 ){
-        perror("file error");
-        return -1 ;
-    }
-
-    if(dup2(fd,STDOUT_FILENO) == -1){
-        perror("dup2 error");
-        close(fd) ;
-        return -1 ;
-    }
-
-    close(fd)  ;
-    return 1 ;
-}
-
-int redirect_input(const RedirectInfo *redir){
-    if(redir->input_file == NULL)   return 0;
-    int fd ;
-    fd = open(redir->input_file, O_RDONLY) ;
-                
-    if(fd == -1 ){
-        perror("file error");
-        exit(1) ;
-    }
-
-    if(dup2(fd,STDIN_FILENO) == -1){
-        perror("dup2 error");
-        close(fd) ;
-        return -1 ;                   
-    }
-
-    close(fd)  ;
-    return 1 ;
-}
-
-
 int main(){
+    Token tokens[MAX_TOKENS] ;
+    Pipeline pipeline ;
     char cmd[1024] = "\0" ;
-    char* token ;
+
     char s[100] ;
     char p_dir[100] ;
     
-    
-
     getcwd(p_dir,sizeof(p_dir)) ;
     getcwd(s,sizeof(s)) ;
 
     while(1){
-        RedirectInfo redir ;
-        redir.input_file = NULL ;
-        redir.output_file = NULL ;
-        redir.rdir_type = NONE ;
+        // core initialization
+        init_tokens(tokens) ;
+        init_pipeline(&pipeline);
 
-        // args split pointers for left and right of |
-        char **left ;
-        char **right ; 
-        bool is_pipe = false ;
-
+        //prompt
         if(strcmp(s,p_dir)==0)
             printf("myshell> ") ;
         else{
@@ -96,152 +38,36 @@ int main(){
         if(fgets(cmd,sizeof(cmd),stdin) == NULL)   break ;
         cmd[strcspn(cmd,"\n")] = '\0' ;
 
-        token = strtok(cmd," ") ;
-
-        if(token == NULL)    continue ;
-
-        int tokenc = 0 ;
-        char* args[100] ;
-
-        while(token != NULL){
-            args[tokenc] = token ;
-            tokenc ++ ;
-            token = strtok(NULL, " ");
-        }
-        args[tokenc] = NULL ;
-
-        tokenc = 0 ;
-        int error = 0 ;
-
-        while(args[tokenc]){
-            if(strcmp(args[tokenc],">")==0){
-                if(args[tokenc+1] == NULL){
-                    printf("error :no file\n") ;
-                    error = 1 ;
-                    break ;
-                }
-                args[tokenc] = NULL ;
-                redir.output_file = args[tokenc+1] ;
-                redir.rdir_type = TRUNCATE ;
-            } else if(strcmp(args[tokenc],">>")==0){
-                if(args[tokenc+1] == NULL){
-                    printf("error :no file\n") ;
-                    error = 1 ;
-                    break ;
-                }
-                args[tokenc] = NULL ;
-                redir.output_file = args[tokenc + 1] ;
-                redir.rdir_type = APPEND ;
-            } else if(strcmp(args[tokenc],"<") == 0){
-                if(args[tokenc+1] == NULL){
-                    printf("error :no file\n") ;
-                    error = 1 ;
-                    break ;
-                }
-                args[tokenc] = NULL ;
-                redir.input_file = args[tokenc+1] ;
-            }else if(strcmp(args[tokenc],"|") == 0){
-                if(args[tokenc+1] == NULL){
-                    printf("error :no file\n") ;
-                    error = 1 ;
-                    break ;
-                }
-                args[tokenc] = NULL ;
-                right = &args[tokenc +1] ;
-                left = args ;
-                is_pipe= true ;
-            }
-            tokenc ++ ;
-        }
-
-
-        if(error == 1 )     continue ;
-
-        // implementing cd command
-
-        if(strcmp(args[0],"cd")==0){
-            if(args[1]!=NULL){
-                if(chdir(args[1])!= 0)
-                    perror("chdir error");
-            }else{
-                chdir(getenv("HOME")) ;
-            }
-            getcwd(s,sizeof(s)) ;
+        // tokenization
+        if(tokenize(cmd ,tokens) == -1){
+            free_tokens(tokens) ;
             continue ;
         }
-
-        if(strcmp(args[0] ,"exit")==0)   break ;
-
-
-        if(is_pipe){
-            pid_t pid1 , pid2 ;
-            int fd[2] ;
-            if(pipe(fd)<0){
-                perror("pipe error") ;
-                continue ;
-            }
-
-            pid1 = fork() ;
-
-            if(pid1 == 0){
-                close(fd[0]);
-
-                dup2(fd[1],STDOUT_FILENO);
-                close(fd[1]) ;
-
-                if(execvp(left[0] ,left)==-1){
-                    perror("execvp fail") ;
-                    exit(1);
-                }
-            }else if(pid1 < 0){
-                perror("fork failed ");
-                _exit(EXIT_FAILURE);
-            }
-
-            pid2 = fork() ;
-
-            if(pid2 == 0){
-                close(fd[1]);
-
-                dup2(fd[0],STDIN_FILENO);
-                close(fd[0]) ;
-
-                if(execvp(right[0] ,right)==-1){
-                    perror("execvp fail") ;
-                    exit(1);
-                }
-            }else if(pid2 < 0){
-                perror("fork failed ");
-                _exit(EXIT_FAILURE);
-            }
-            close(fd[0]);
-            close(fd[1]);
-            waitpid(pid1,NULL,0);
-            waitpid(pid2,NULL,0) ;
+        
+        // parsing
+        if(parse_pipeline(tokens,&pipeline) == -1){
+            free_tokens(tokens) ;
             continue ;
         }
-
-        pid_t pid = fork() ;
-
-        if(pid == 0){
-
-            // implementing file descriptor
-
-            redirect_output(&redir);
-            redirect_input(&redir);
-
-            if(execvp(args[0],args) == -1 ){
-                perror("execvp fail") ;
-                exit(1);
-            }
-        }else if(pid < 0){
-            perror("Fork failed") ;
-            _exit(EXIT_FAILURE);
+        
+        // empty command
+        if(pipeline.command_count == 0){     
+            free_tokens(tokens) ;
+            continue ;
         }
+        
+        // single and builtin command execution
+        if(pipeline.command_count == 1 && is_builtin(&pipeline.commands[0])){
+                execute_builtin(&pipeline.commands[0]) ;
+                getcwd(s, sizeof(s));
+        }
+        // command execution
         else{
-	        int status ;	
-            waitpid(pid,&status,0) ;
+            execute_pipeline(&pipeline) ;
         }
+
+        free_tokens(tokens);
+        
     }
     return 0 ;
 }
