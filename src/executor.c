564bd3f6697091d8b1febd72f99ed3acff65b3d8
MananBisht <bishtmanan205@gmail.com>
2026-07-30 13:55:03 -0400

    N commands pipeline

--- src/executor.c
+++ src/executor.c
@@ -27,83 +27,79 @@ int execute_command(const Command *cmd){
         return -1 ;
     }else{
         int status ;
-        waitpid(pid , &status , NULL) ;
+        waitpid(pid , &status , 0) ;
     }
-    return 0 ;
+    return 1 ;
 }
 
-
+// N command pipe execution 
 int execute_pipe(const Pipeline *pipeline){
-    // for two commands only (e.g. ls | grep txt)
-    pid_t pid1,pid2 ;
-    int fd[2] ;
-    if(pipe(fd) < 0){
-        perror("pipe errro : ");
-        _exit(EXIT_FAILURE) ;
+    int prev_read = -1 ;
+    int command = pipeline->command_count ;
+    pid_t pids[command] ;
+
+    for(int i = 0 ; i < command ; i ++){
+        int fd[2] = {-1,-1} ;
+
+        if(i != command-1 ){
+            if(pipe(fd) == -1){
+                perror("pipe error ") ;
+                return -1  ;
+            }
+        }
+
+        pid_t pid = fork() ;
+
+        if(pid == -1 ){
+            perror("fork error ") ;
+            return -1 ;
+        }
+
+        if(pid == 0){
+            if(i > 0){
+                if(dup2(prev_read,STDIN_FILENO) == -1){
+                    perror("dup2 error ") ;
+                    _exit(EXIT_FAILURE);
+                }
+            }
+            if( i < command -1){
+                if(dup2(fd[1],STDOUT_FILENO) == -1){
+                    perror("dup2 error ") ;
+                    _exit(EXIT_FAILURE);
+                }
+            }
+
+            if(fd[1] != -1)     close(fd[1]) ;
+            if(fd[0] != -1)     close(fd[0]) ;
+            if(prev_read != -1 )close(prev_read ) ;
+
+            if(redirect_input(&pipeline->commands[i] )== -1)
+                _exit(EXIT_FAILURE) ;
+            if(redirect_output(&pipeline->commands[i] ) == -1 )
+                _exit(EXIT_FAILURE) ;
+            
+            if(execvp(pipeline->commands[i].args[0],pipeline->commands[i].args) == -1){
+                perror("execvp error ");
+                _exit(EXIT_FAILURE);
+            }
+        }
+
+        pids[i] = pid ;
+
+        if(prev_read != -1)        close(prev_read) ;
+
+        if( i < command -1 ){
+            prev_read = fd[0] ;
+            close(fd[1]) ;
+        }
     }
+    if(prev_read != -1)   close(prev_read) ;  
+    int status;
 
-    pid1 = fork() ;
-
-    if(pid1 == 0 ){
-        close(fd[0]) ;
-        if(dup2(fd[1],STDOUT_FILENO) < 0 ){
-            perror("dup2 error : ");
-            close(fd[1]);
-            _exit(EXIT_FAILURE) ;
-        }
-        close(fd[1]) ;
-
-        if(redirect_input(&pipeline->commands[0]) == -1)
-            _exit(EXIT_FAILURE);
-        if(redirect_output(&pipeline->commands[0]) == -1)
-            _exit(EXIT_FAILURE) ;
-
-        if(execvp(pipeline->commands[0].args[0],pipeline->commands[0].args) == -1){
-            perror("execvp error : ") ;
-            _exit(EXIT_FAILURE);
-        }
-    }else if( pid1 < 0 ){
-        perror("fork error : ") ;
-        close(fd[0]) ;
-        close(fd[1]) ;
-        return -1 ;
-    }
-
-    pid2 = fork() ;
-
-    if(pid2 == 0 ){
-        close(fd[1]) ;
-        if(dup2(fd[0],STDIN_FILENO) < 0 ){
-            perror("dup2 error : ");
-            close(fd[0]);
-            _exit(EXIT_FAILURE) ;
-        }
-        close(fd[0]) ;
-
-        if(redirect_input(&pipeline->commands[1]) == -1)
-            _exit(EXIT_FAILURE);
-        if(redirect_output(&pipeline->commands[1]) == -1)
-            _exit(EXIT_FAILURE) ;
-
-
-        if(execvp(pipeline->commands[1].args[0],pipeline->commands[1].args) == -1){
-            perror("execvp error : ") ;
-            _exit(EXIT_FAILURE);
-        } 
-    }else if( pid2 < 0 ){
-        perror("fork error : ") ;
-        close(fd[0]) ;
-        close(fd[1]) ;
-        return -1 ;
-    } 
-        close(fd[0]) ;
-        close(fd[1]) ;
-        int status ;
-        waitpid(pid1 , &status , 0) ;
-        waitpid(pid2 , &status , 0) ;
-
-        return 0 ;
-
+    for(int i = 0 ; i < command ; i ++ )
+        waitpid(pids[i] , &status, 0 ) ;
+    
+    return 0 ;
 }
 
 int execute_pipeline(const Pipeline *pipeline){
